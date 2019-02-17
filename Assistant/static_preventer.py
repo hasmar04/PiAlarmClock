d6b9e93c9db85cb9011226a32c0d1f744d212629
Harrison Asmar <34726416+hasmar04@users.noreply.github.com>
2019-02-17 10:32:39 +1000

    Add files via upload

---
+++ Assistant/static_preventer.py
@@ -0,0 +1,17 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(20,GPIO.OUT)
+print("Startup Complete")
+
+while 1:
+    fh=open("/proc/asound/card1/pcm0p/sub0/status","r")
+    sound = fh.read()
+    sound = sound.strip()
+    fh.close()
+    if (sound == "closed"):
+        GPIO.output(20,GPIO.LOW)
+    else:
+        GPIO.output(20,GPIO.HIGH)
