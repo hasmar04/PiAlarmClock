f20509b7c01e47a363242dd99891546b098540b5
Harrison Asmar <34726416+hasmar04@users.noreply.github.com>
2019-02-17 10:40:44 +1000

    Update static_preventer.py

--- Static Prevention/static_preventer.py
+++ Static Prevention/static_preventer.py
@@ -7,7 +7,7 @@ GPIO.setup(20,GPIO.OUT)
 print("Startup Complete")
 
 while 1:
-    fh=open("/proc/asound/card1/pcm0p/sub0/status","r")
+    fh=open("/proc/asound/card1/pcm0p/sub0/status","r") # replace 1 in card1 with number from aplay -l
     sound = fh.read()
     sound = sound.strip()
     fh.close()
