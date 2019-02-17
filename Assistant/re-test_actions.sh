061f2bbb3a33dd089b4a4c72efad8619fca11dc6
Harrison Asmar <34726416+hasmar04@users.noreply.github.com>
2019-02-17 10:28:18 +1000

    Update re-test_actions.sh
    Updated to update RadioStart, RadioPlay removed

--- Assistant/re-test_actions.sh
+++ Assistant/re-test_actions.sh
@@ -1,2 +1,2 @@
 ./gactions test --action_package radiostop.json --project <your-project-name>
-./gactions test --action_package radioplay.json --project <your-project-name>
+./gactions test --action_package radiostart.json --project <your-project-name>
